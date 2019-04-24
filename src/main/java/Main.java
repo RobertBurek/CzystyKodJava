52fb86d3001f00d60ba85c09429ce185a0fb4866
Robert Burek <robertburek@wp.pl>
2019-04-24 12:11:16 +0200

    Spaghetti code - trochę bardziej czytelniejszy ale jeszcze dużo brakuje.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
-import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
+//Zachłannego importu należy unikać w aplikacjach webowych
+//import java.util.*;
 
 /**
  * Created by Robert Burek
  */
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         // wygenerowanie danych
-        ArrayList<String> daneKontaktowe = new ArrayList<String>(Arrays.asList(
+        Set<String> daneKontaktowe = new HashSet<String>(Arrays.asList(
                 "Anna Janicka a.janicka@mail.pl",
                 "Jan Janicka j.jan@wp.pl",
                 "Paweł Borek pawelpawelek@mmail.pl"
@@ -22,7 +24,7 @@ public class Main {
         String query = scanner.nextLine();
 
         // wykonanie wyszukiwania
-        for(String string: daneKontaktowe){
+        for (String string : daneKontaktowe) {
             if (string.contains(query)) {
                 // wypisanie wyników
                 System.out.println(string);
